1c6ee5d584e0f6ed3a38344c96b90ea29d498136
Arthur <sodrearthur2@gmail.com>
2022-03-09 12:55:03 -0300

    second commit

--- tratamento.py
+++ tratamento.py
@@ -1,16 +1,27 @@
 import numpy as np
 import pandas as pd
+import os
 
-def amostras_import():
-    amostras_p = pd.read_excel("compilado_positivo.xlsx")
-    amostras_p.set_index('Wavelength',inplace=True)
-    amostras_p = amostras_p.T
-    amostras_p['Diagnostico'] = 'Positivo'
-    amostras_n = pd.read_excel("compilado_negativo.xlsx")
-    amostras_n.set_index('Wavelength',inplace=True)
-    amostras_n = amostras_n.T
-    amostras_n['Diagnostico'] = 'Negativo'
-    amostras = amostras_p.append(amostras_n)
-    return amostras
+
+class Tratamento(self):
+
+    def __init__(self):
+        self.path_positivo =  'Dados/compilado_positivo.xlsx'
+        self.path_negativo = 'Dados/compilado_negativo.xlsx'
+
+
+
+    def amostras_import(self):
+        amostras_p = pd.read_excel(self.path_positivo,engine='openpyxl')
+        amostras_p.set_index('Wavelength',inplace=True)
+        amostras_p = amostras_p.T
+        amostras_p['Diagnostico'] = 'Positivo'
+        amostras_n = pd.read_excel(self.path_negativo,engine='openpyxl')
+        amostras_n.set_index('Wavelength',inplace=True)
+        amostras_n = amostras_n.T
+        amostras_n['Diagnostico'] = 'Negativo'
+        amostras = amostras_p.append(amostras_n)
+        
+        return amostras
 
 
