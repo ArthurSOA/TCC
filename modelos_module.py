e167ae7159f13328cde121ae96cc9246d2d8ee2b
Arthur <sodrearthur2@gmail.com>
2022-06-16 20:13:53 -0300

    last commit

--- modelos_module.py
+++ modelos_module.py
@@ -1,10 +1,14 @@
 
 import scipy as sp
 import tensorflow as tf
-
+import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+
+
+
 
 
 
@@ -14,20 +18,33 @@ from tensorflow.keras.layers import Dense
 
 class Modelos:
 
-    def __init__(self,X_train,y_train):
-        self.X_train = X_train
-        self.y_train = y_train
+    def __init__(self,X_train,y_train, X_val = None,y_val = None):
+        if isinstance(X_train, pd.DataFrame):
+            self.X_train = X_train.to_numpy()
+            self.y_train = y_train.to_numpy()
+            if X_val is not None:
+                self.X_val = X_val.to_numpy()
+                self.y_val = y_val.to_numpy()
+        else:
+            self.X_train = X_train
+            self.y_train = y_train.to_numpy()
+            if X_val is not None:
+                self.X_val = X_val
+                self.y_val = y_val.to_numpy()
+
+
+       
         
 
     def SVM(self):
-        clf_svm = SVC(random_state = 42)
-        clf_svm.fit(self.X_train.to_numpy(),self.y_train.to_numpy())
+        clf_svm = SVC(random_state = 42,kernel='rbf')
+        clf_svm.fit(X=self.X_train,y=self.y_train)
         return clf_svm
         
     def SVM_GRID(self):
         param_grid =  {
-                'C':[0.5,1,10,100],
-                'gamma':['scale',1,0.1,0.01,0.001,0.0001],
+                'C':[0.1,0.3,0.5,1,10,100],
+                'gamma':['scale',1,0.1,0.01,0.001],
                 'kernel':['rbf']
             }
         
@@ -39,13 +56,109 @@ class Modelos:
 
         return clf
 
-    def NN(self):
+
+    def grid_nn(self,neuronios: list = [10,20,30,40,50,80]):  
+        modelos = []
+        for i in neuronios:
+            modelos.append(self.NN(i))
+
+        return modelos
+
+
+    def NN(self,camada2):
+
+
         model = tf.keras.models.Sequential()
-        model.add(Dense(10,activation = "relu",name = "layer1"))
-        model.add(Dense(5,activation = "relu"))
-        model.add(Dense(1,activation = 'softplus'))
-        opt = tf.keras.optimizers.SGD(lr = 0.001)
+        model.add(Dense(20,activation = 'relu',input_dim = self.X_train.shape[1]))
+        model.add(Dense(camada2,activation = 'relu'))
+        #model.add(Dropout(0.5))
+        #model.add(Dense(40,activation = "relu"))
+        #model.add(Dropout(0.5))
+        #model.add(Dense(20,activation = "relu"))
+        #model.add(Dropout(0.5))
+        #model.add(Dense(30,activation = 'relu'))
+        #model.add(Dropout(0.5))
+        model.add(Dense(1,activation = 'sigmoid'))
+        #opt = tf.keras.optimizers.SGD(lr = 0.001)
+        opt = tf.keras.optimizers.Adam()
         model.compile(optimizer = opt,loss = 'binary_crossentropy',metrics=  ['accuracy'])
-        model.fit(self.X_train.to_numpy().astype(float),self.y_train.to_numpy().astype(int),epochs = 20,verbose= 0)
+        model.fit(self.X_train.astype(float),self.y_train.astype(int),epochs = 300,verbose= False)
 
         return model
+
+
+
+    def NN_s(self,camada2):
+
+
+        model = tf.keras.models.Sequential()
+        model.add(Dense(10,activation = "relu",input_dim = self.X_train.shape[1]))
+        model.add(Dense(camada2,activation = "relu"))
+        model.add(Dense(5,activation = "relu"))
+        model.add(Dense(1,activation = "sigmoid"))
+        #opt = tf.keras.optimizers.SGD(lr = 0.001)
+        opt = tf.keras.optimizers.Adam()
+        model.compile(optimizer = opt,loss = 'binary_crossentropy',metrics=  ['accuracy'])
+        model.fit(self.X_train.astype(float),self.y_train.astype(int),epochs = 300,verbose= False)
+
+        return model        
+
+
+
+
+
+    def model_ff(self,epochs = 500):
+        model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(512, input_shape=(self.X_train.shape[1],), activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(1,activation = 'sigmoid') 
+        ])
+
+        es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=250, verbose=1, mode='auto', restore_best_weights=True)
+        model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy', metrics=  ['accuracy'])
+        history = model.fit(self.X_train, self.y_train, validation_data=(self.X_val, self.y_val), epochs=epochs,callbacks = [es])
+
+        return history
+
+
+    def model_conv(self):
+        model = tf.keras.models.Sequential([
+        tf.keras.layers.Reshape(target_shape=[216, 1], input_shape=(216,)),
+        tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPool1D(pool_size=2),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPool1D(pool_size=2),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPool1D(pool_size=2),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.Conv1D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        tf.keras.layers.MaxPool1D(pool_size=2),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(1)
+    ])
+        model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='mse')
+        history = model.fit(self.X_train, self.y_train,  epochs=1000, batch_size=32)
+        return history
+
+
+
+
+
+
--- tratamento_module.py
+++ tratamento_module.py
@@ -1,4 +1,6 @@
+from ctypes.wintypes import BOOLEAN
 from pyexpat.errors import XML_ERROR_NOT_STANDALONE
+from turtle import color
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -6,6 +8,17 @@ import os
 import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
 from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+import imblearn
+from sklearn.metrics import PrecisionRecallDisplay
+from sklearn.metrics import RocCurveDisplay
+from sklearn import metrics
+from scipy.signal import savgol_filter
+
+
+
+
+
 class Tratamento:
 
     def __init__(self):
@@ -36,6 +49,24 @@ class Tratamento:
             amostras = amostras[freq]
         return amostras
 
+    def get_frequencies_from(self,amostras,freq):
+        if freq > 350:
+            freq = freq-350
+        else:
+            return
+        columns = amostras.drop(columns='Diagnostico').columns.to_numpy()
+        columns = columns[freq:,]
+        columns = np.append(columns,'Diagnostico')
+
+        amostras = amostras[columns]
+
+
+        
+        return amostras
+ 
+
+
+
     def get_samples(self,amostras):
         self.X = amostras.drop('Diagnostico',axis=1).copy()
         self.y = amostras['Diagnostico'].copy()
@@ -43,19 +74,101 @@ class Tratamento:
 
         return self.X,self.y
 
-    def training_test(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X,self.y,test_size = 0.1,random_state = 42)
-        
-        return X_train,X_test,y_train,y_test
 
-    def confusion(self,y_true,y_predict):
+    def training_test(self,X ,y ):
+
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y,test_size = 0.3,random_state = 37)
+
+        return self.X_train,self.X_test,self.y_train,self.y_test
+
+
+
+    def under_sample(self,X_train =  None,y_train = None,sampling_strategey = "majority"):
+        if X_train is None:
+            X_train = self.X_train
+        if y_train is None:
+            y_train = self.y_train
+
+        
+        
+        undersampler = imblearn.under_sampling.RandomUnderSampler(sampling_strategy = sampling_strategey)
+        self.X_under, self.y_under = undersampler.fit_resample(self.X_train,self.y_train)
+
+        return self.X_under, self.y_under
+
+    def over_sample(self,X_train = None, y_train = None):
+        if X_train is None:
+            X_train = self.X_train
+        if  y_train is None:
+            y_train = self.y_train
+
+        
+        oversampler = imblearn.over_sampling.RandomOverSampler()
+        self.X_over, self.y_over = oversampler.fit_resample(X_train,y_train)
+
+        return self.X_over,self.y_over
+        
+
+
+    def PCA_t(self,X_train,n_components = 4):
+        pca = PCA(n_components = n_components)
+        if isinstance(X_train, pd.DataFrame):
+            pca.fit(X_train.to_numpy())
+        else:
+            pca.fit(X_train)
+        
+
+        return pca
+        
+
+    def savgol(X_values, window_lenght = 11, polyorder =5, deriv = 1):
+
+        x_sav = savgol_filter(x=X_values,window_length=window_lenght,polyorder=polyorder,deriv=deriv)
+
+        return x_sav
+
+
+    def confusion(self,y_true,y_predict,fig_name ="confusion.png" ):
         cm = confusion_matrix(y_true,y_predict)
         class_names= ['Negativo','Positivo']
-        fig, ax = plot_confusion_matrix(conf_mat=cm, figsize=(6, 6), cmap=plt.cm.Greens,
+        fig, ax = plot_confusion_matrix(conf_mat=cm, figsize=(2, 2), cmap=plt.cm.Greens,
                                         class_names = class_names)
-        plt.xlabel('Prredições', fontsize=18)
-        plt.ylabel('Verdadeiros', fontsize=18)
-        plt.title('Matrix de confusão', fontsize=18)
+        ax
+        plt.xlabel('Predições', fontsize=12)
+        plt.ylabel('Verdadeiros', fontsize=12)
+        plt.title('Matriz de confusão', fontsize=12)
+        plt.savefig(fig_name, bbox_inches = 'tight')
         plt.show()
+        
+        return
 
+    def precisionrecall(self,y_true,y_predict,name = "SVC"):
+
+        display = PrecisionRecallDisplay.from_estimator(
+            classifier, X_test, y_test, name=name
+        )
+        _ = display.ax_.set_title("2-class Precision-Recall curve")
+
+    def roc(self,y,pred,estimator_name: str = 'None'):
+        fpr, tpr, thresholds = metrics.roc_curve(y, pred)
+        roc_auc = metrics.auc(fpr, tpr)
+ 
+        
+        return fpr,tpr,thresholds,roc_auc
+
+    
+    def plot_roc(self,fpr: list, tpr: list, Names: list, name_curve: str, auc: list, tpr_threshold = 0.8,):
+        colors = ['r','b']
+        plt.figure(figsize = (8,8))
+        for i in range(len(fpr)):
+            plt.plot(fpr[i], tpr[i], colors[i],label=Names[i] + " Área: " + str(auc[i]))
+       
+        plt.legend(loc="lower right")
+        plt.title("Curva ROC RNA" + " "+ name_curve, fontsize = 15)
+        plt.xlabel("Falsos Positivos",fontsize = 15)
+        plt.ylabel("Verdadeiros Positivos", fontsize = 15)
+        plt.grid()
+        plt.savefig(name_curve+'.png', bbox_inches = 'tight')
+        plt.show()
 
